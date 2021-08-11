963b8f3815eb6362ce87e39014291642ae9decb1
Alesya <alesya_aleshkevich@mail.ru>
2021-08-11 16:51:25 +0300

    start 10 task

--- task2/script.js
+++ task2/script.js
@@ -75,6 +75,7 @@ const arr = [
         production: "Heyday Films, Warner Bros.",
     },
     {
+        id: 5,
         title: "Harry Potter and the Sorcerer's Stone",
         year: 2001,
         released: "16 Nov 2001",
@@ -149,3 +150,7 @@ console.log(getFilm(arr, "school"));
 //Создать функцию, которая бы принимала 3 параметра:  1)массив фильмов , 2) строка(название поля, например 'title') и строку/число(значение поля "Black Widow"). А результатом этой функции должен быть отфильтрованный массив, где параметры 2 и 3 равны в объекте фильма. 
 /* Например: передаем `(films, 'title', 'Black Widow')` и на выходе получаем фильм с id=1
 если передаем `(films, 'year', 2011)` , то получаем фильм с id=2 */
+function getAttribute(arr, key, value) {
+    const attribute = 
+}
+console.log(getAttribute(arr, 'title', 'Black Widow'));
